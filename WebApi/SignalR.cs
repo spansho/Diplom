9e0170414920f2cfcaf733daa15759f4a7f0911e
egor <@DESKTOP-M4T0586>
2023-06-17 18:51:02 +0300

    Revert "hghg"
    This reverts commit efd36ecda7c45fc8e34f48b06eb62492aa9ea8c8.

--- WebApi/SignalR.cs
+++ WebApi/SignalR.cs
@@ -51,11 +51,12 @@ namespace ServerSite
             await Clients.Group(roomId).SendAsync("ChangingEstimate", _repository.RoomUser.GetAllRoomUsers(false, roomId));
         }
 
-        public async Task Voting(string estimate, string userId, string roomId,string issueId, string token = "")
+        public async Task Voting(string estimate, string userId, string roomId, string token = "")
         {
             if(token!=string.Empty)
             {
-                AuthorizedUserIssue issue= new AuthorizedUserIssue() {UserId=Guid.Parse(userId),IssueId= issueId, Estimation=int.Parse(estimate) };
+                //хз как тут текущую задачу полоучить ну потом обсудим
+                AuthorizedUserIssue issue= new AuthorizedUserIssue() {UserId=Guid.Parse(userId),IssueId="хз что сюда ебануть",Estimation=int.Parse(estimate) };
                 _repository.authorizedUserIssue.CreateAuthorizedUserIsse(issue);
             }    
             var roomUser = _repository.RoomUser.GetRoomUserById(userId);
@@ -80,7 +81,7 @@ namespace ServerSite
             if(!string.IsNullOrEmpty(issueId))
             {
                 var issue = _repository.Issue.GetIssueById(issueId);
-                issue.Estimation = estimation;
+                issue.Estimation = int.Parse(estimation);
             }
 
             _repository.Save();
@@ -89,7 +90,7 @@ namespace ServerSite
 
         public async Task CreateNewIssueAsync(string roomId, string name)
         {
-            var issue = new Issue { Name = name, Description = string.Empty, Priority = "PP-2", RoomId = roomId, Link = string.Empty, IssueId = Guid.NewGuid().ToString(), Estimation = string.Empty, CreatingTime = DateTime.Now };
+            var issue = new Issue { Name = name, Description = string.Empty, Priority = "PP-2", RoomId = roomId, Link = string.Empty, IssueId = Guid.NewGuid().ToString(), Estimation = 0, CreatingTime = DateTime.Now };
             _repository.Issue.CreateIssue(issue);
             _repository.Save();
 
